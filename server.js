1515b358ff755cc65abb614bde7e3abed9409624
Osama Sehgol <osama.sehgol@gmail.com>
2016-06-25 09:03:00 -0400

    update twitter working and basic dashboard build

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 //HTTP portion
+// var express = require('express');
+// var app = express();
 var http = require('https');
 var fs = require('fs');
 var url = require('url');
 var Twitter = require('twitter');
 //var Keys = require('./keys.js');
 var oauth = require('oauth');
+var tagSearch = null;
+
 
 var options = {
   key: fs.readFileSync('my-key.pem'),
@@ -17,8 +21,8 @@ httpServer.listen(8080, function(){
 });
 
 function requestHandler(req, res){
-	
-		var parsedURL = url.parse(req.url);
+
+		var parsedUrl = url.parse(req.url);
 				console.log("The request is: "+parsedUrl.pathname);
 		
 		fs.readFile(__dirname + parsedUrl.pathname, function(err, data) {
@@ -42,35 +46,37 @@ var twitter_client = new Twitter({
 
 	consumer_key: 'ZWdQIOm2KCxKuaoV1foqkA4Yn',
 	consumer_secret: 'iOVj78uN3GY63k181ZxjOCQf1gWy5cw2oEsHGt2Gn9PJ4RwzIq',
-	access_token: '102290558-Wb0q9Hm4Jvp9LtLigSFF8RDXWJeA6qG6KrHKRhZ8',
+	access_token_key: '102290558-Wb0q9Hm4Jvp9LtLigSFF8RDXWJeA6qG6KrHKRhZ8',
 	access_token_secret: 'JtZzfuWC3OAGkUxvLzG57C0YSA7J8oITjLLIEcSTuxmkW'
 
 });
 
 // scraping tweets for #'tags'
-twitter_client.get('search/tweets', {q:"#witness"}, function(error, tweets, response){
+twitter_client.get('search/tweets', {q:'#witness'}, function(error, tweets, response){
 	
 	console.log(tweets);
 
-	//pick a tweet at random
-	// var tweet = tweets.statuses[Math.floor(Math.random()*tweets.statuses.length)];
+	// pick a tweet at random
+	var tweet = tweets.statuses[Math.floor(Math.random()*tweets.statuses.length)];
 
 
-	// // post a reply to 
-	// client.post('statuses/update', {status: "@"+tweet.user.screen_name+" we're broadcasting you now", in_reply_to_status_id: tweet.in_reply_to_status_id				
-	// 								}, function(error, tweet, response) {
-	// 									if (!error) {
-	// 										console.log(tweet);
-	// 									} else {
-	// 										console.log(error);
-	// 									}
-	// 								});
+	// post a reply to 
+	twitter_client.post('statuses/update', {status: "@"+tweet.user.screen_name+" we're broadcasting you now", in_reply_to_status_id: tweet.in_reply_to_status_id				
+									}, function(error, tweet, response) {
+										if (!error) {
+											console.log(tweet);
+										} else {
+											console.log(error);
+										}
+									});
 
 
  });
 
 
 
+//socket portion
+var io = require('socket.io').listen(httpServer);
 
 
 
