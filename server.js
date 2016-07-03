ace4bcd31e664ef9956079dc9756f70652a9486c
Osama Sehgol <osama.sehgol@gmail.com>
2016-07-03 17:41:14 -0400

    #twitter search

--- server.js
+++ server.js
@@ -39,39 +39,7 @@ function requestHandler(req, res){
 
 )};
 
-//Twitter portion
 
-//Twitter configuration
-var twitter_client = new Twitter({
-
-	consumer_key: 'ZWdQIOm2KCxKuaoV1foqkA4Yn',
-	consumer_secret: 'iOVj78uN3GY63k181ZxjOCQf1gWy5cw2oEsHGt2Gn9PJ4RwzIq',
-	access_token_key: '102290558-Wb0q9Hm4Jvp9LtLigSFF8RDXWJeA6qG6KrHKRhZ8',
-	access_token_secret: 'JtZzfuWC3OAGkUxvLzG57C0YSA7J8oITjLLIEcSTuxmkW'
-
-});
-
-// scraping tweets for #'tags'
-twitter_client.get('search/tweets', {q:'#witness'}, function(error, tweets, response){
-	
-	console.log(tweets);
-
-	// pick a tweet at random
-	var tweet = tweets.statuses[Math.floor(Math.random()*tweets.statuses.length)];
-
-
-	// post a reply to 
-	twitter_client.post('statuses/update', {status: "@"+tweet.user.screen_name+" we're broadcasting you now", in_reply_to_status_id: tweet.in_reply_to_status_id				
-									}, function(error, tweet, response) {
-										if (!error) {
-											console.log(tweet);
-										} else {
-											console.log(error);
-										}
-									});
-
-
- });
 
 
 
@@ -81,7 +49,49 @@ var io = require('socket.io').listen(httpServer);
 io.sockets.on('connection', function(socket){
 
 	socket.on('hash-tag-search', function(data){
-	console.log(data);
+	console.log(data.text);
+
+	//Twitter portion
+
+		//Twitter configuration
+		var twitter_client = new Twitter({
+
+			consumer_key: 'ZWdQIOm2KCxKuaoV1foqkA4Yn',
+			consumer_secret: 'iOVj78uN3GY63k181ZxjOCQf1gWy5cw2oEsHGt2Gn9PJ4RwzIq',
+			access_token_key: '102290558-Wb0q9Hm4Jvp9LtLigSFF8RDXWJeA6qG6KrHKRhZ8',
+			access_token_secret: 'JtZzfuWC3OAGkUxvLzG57C0YSA7J8oITjLLIEcSTuxmkW'
+
+		});
+
+		// scraping tweets for #'tags'
+		twitter_client.get('search/tweets', {q: data.text}, function(error, tweets, response){
+			
+			console.log(tweets);
+
+			// pick a tweet at random
+			var tweet = tweets.statuses[Math.floor(Math.random()*tweets.statuses.length)];
+
+
+			// post a reply to 
+			twitter_client.post('statuses/update', {status: "@"+tweet.user.screen_name+" we're broadcasting you now", in_reply_to_status_id: tweet.in_reply_to_status_id				
+											}, function(error, tweet, response) {
+												if (!error) {
+													console.log(tweet);
+												} else {
+													console.log(error);
+												}
+											});
+
+
+		 });
+
+
+
+
+
+
+
+
 	});
 
 
